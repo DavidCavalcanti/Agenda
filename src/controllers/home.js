994ac333e5799747f71624d46c44ec4d1ad4da9a
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-14 17:53:57 -0300

    Limpar módulos desnecessários

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,14 +1,5 @@
 const HomeModel = require("../models/homeModel");
 
-exports.paginaHome = (req, res) => {
-  res.render("index", {
-    titulo: "Este será o título da página",
-    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  });
-
-  return;
-};
-
-exports.trataPost = (req, res) => {
-  res.send(req.body);
+exports.index = (req, res) => {
+  res.render("index");
 };
