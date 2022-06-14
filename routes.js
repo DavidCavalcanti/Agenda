6396448aa6f4a95e185ec83039ebe902fa92f8ef
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-14 17:54:15 -0300

    Limpar rotas desnecessárias

--- routes.js
+++ routes.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const route = express.Router();
 const paginaHome = require("./src/controllers/home");
-const paginaContato = require("./src/controllers/contato");
 
 // Rota da página home
-route.get("/", paginaHome.paginaHome);
-route.post("/", paginaHome.trataPost);
-
-// Rota da página contato
-route.get("/contato", paginaContato.paginaContato);
-route.post("/contato", paginaContato.trataForm);
+route.get("/", paginaHome.index);
 
 module.exports = route;
