f53fa7aa4b3e35282ece4c61ebcc8d40e50e0789
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-23 17:22:06 -0300

    Criar rota para login

--- routes.js
+++ routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const route = express.Router();
 const paginaHome = require("./src/controllers/home");
+const paginaLogin = require("./src/controllers/login");
 
 // Rota da página home
 route.get("/", paginaHome.index);
+
+//Rotas de  login
+route.get("/login/index", paginaLogin.index);
 
 module.exports = route;
