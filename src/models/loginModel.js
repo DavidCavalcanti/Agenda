4ba37d1e5323c09046f758b4a0a810b454e2b53d
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-23 17:51:18 -0300

    Criar model  para login

---
+++ src/models/loginModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+//modelagem dos dados
+const LoginSchema = new mongoose.Schema({
+  // mandando cofiguração dos dados que queremos
+  titulo: { type: String, required: true },
+  descricao: String,
+});
+
+const LoginModel = mongoose.model("Login", LoginSchema); // cria um model por nome Login e passar o esquema utilizado nele
+
+class Login {
+  constructor(body){
+    this.body = body;
+  }
+}
+
+module.exports = Login;
