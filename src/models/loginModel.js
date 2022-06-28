37a771324deacdfaedb854c77b7f5c88c67cfbb1
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-06-28 16:23:38 -0300

    Alterar model para validação

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 //modelagem dos dados
 const LoginSchema = new mongoose.Schema({
   // mandando cofiguração dos dados que queremos
-  titulo: { type: String, required: true },
-  descricao: String,
+  email: { type: String, required: true },
+  senha: { type: String, required: true },   
 });
 
 const LoginModel = mongoose.model("Login", LoginSchema); // cria um model por nome Login e passar o esquema utilizado nele
 
 class Login {
-  constructor(body){
+  constructor(body) {
     this.body = body;
-    this.erros = [];    
+    this.erros = [];
+    this.user = null;
   }
 }
 
